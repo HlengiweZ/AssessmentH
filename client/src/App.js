b0735c585e190ae1dd1f628ba87ff6d322feff81
HlengiweZ <zuluhlengiwe2580@gmail.com>
2022-11-28 11:46:46 +0200

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import { BrowserRouter, Switch, Route}  from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Home from "./pages/home";
 
+;
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App">
-      < ToastContainer position ="top-center"/>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </div>
-    </BrowserRouter>
-    );
+   <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/create" component={create} />
+      <Route exact path="/edit/:id" component={edit} />
+      <Route exact path="/view/:id" component={details} /> */}
+    </Switch>
+   </BrowserRouter>
+  );
 }
 
 export default App;
